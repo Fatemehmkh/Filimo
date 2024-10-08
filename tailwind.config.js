6443a03c3d5c24280e06ab690577e2ae7d1cd75c
Fatemehmkh <moslemkhanifateme@gmail.com>
2024-10-08 13:11:03 +0330

    version 07 adding filter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}","./node_modules/flowbite/**/*.js"],
   theme: {
     fontFamily: {
       filimo: "filimo",
@@ -14,5 +14,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
